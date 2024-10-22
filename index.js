4b721e59ca1454397b607b15392323d2e4ade6f2
sahayaprincy <146539309+sahayaprincy@users.noreply.github.com>
2024-10-22 16:51:44 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,21 +3,45 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.roles = exports.roleElements = exports.elementRoles = exports.dom = exports.aria = void 0;
-var _ariaPropsMap = _interopRequireDefault(require("./ariaPropsMap"));
-var _domMap = _interopRequireDefault(require("./domMap"));
-var _rolesMap = _interopRequireDefault(require("./rolesMap"));
-var _elementRoleMap = _interopRequireDefault(require("./elementRoleMap"));
-var _roleElementMap = _interopRequireDefault(require("./roleElementMap"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+exports.default = void 0;
+Object.defineProperty(exports, "specialChars", {
+  enumerable: true,
+  get: function () {
+    return _keyboard.specialCharMap;
+  }
+});
 
-var aria = _ariaPropsMap.default;
-exports.aria = aria;
-var dom = _domMap.default;
-exports.dom = dom;
-var roles = _rolesMap.default;
-exports.roles = roles;
-var elementRoles = _elementRoleMap.default;
-exports.elementRoles = elementRoles;
-var roleElements = _roleElementMap.default;
-exports.roleElements = roleElements;
+var _click = require("./click");
+
+var _type = require("./type");
+
+var _clear = require("./clear");
+
+var _tab = require("./tab");
+
+var _hover = require("./hover");
+
+var _upload = require("./upload");
+
+var _selectOptions = require("./select-options");
+
+var _paste = require("./paste");
+
+var _keyboard = require("./keyboard");
+
+const userEvent = {
+  click: _click.click,
+  dblClick: _click.dblClick,
+  type: _type.type,
+  clear: _clear.clear,
+  tab: _tab.tab,
+  hover: _hover.hover,
+  unhover: _hover.unhover,
+  upload: _upload.upload,
+  selectOptions: _selectOptions.selectOptions,
+  deselectOptions: _selectOptions.deselectOptions,
+  paste: _paste.paste,
+  keyboard: _keyboard.keyboard
+};
+var _default = userEvent;
+exports.default = _default;
